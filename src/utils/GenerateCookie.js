5820f49c715398821fd0cdba8b389d0d26f04947
Rodion Matvieiev <your.email@example.com>
2024-07-16 02:00:46 -0400

    updated LoginController and RefreshController with res.header Access-Control-Allow-Origin and Access-Control-Allow-Credentials

--- src/utils/GenerateCookie.js
+++ src/utils/GenerateCookie.js
@@ -1,13 +1,28 @@
 import { COOKIE, TIME_DURATION } from '../constants/constants.js';
 
+// export const GenerateCookie = (session, res) => {
+//   res.cookie(COOKIE.REFRESH_TOKEN, session.refreshToken, {
+//     httpOnly: true,
+//     expires: new Date(Date.now() + TIME_DURATION.THIRTY_DAYS),
+//   });
+
+//   res.cookie(COOKIE.SESSION_ID, session.id, {
+//     httpOnly: true,
+//     expires: new Date(Date.now() + TIME_DURATION.THIRTY_DAYS),
+//   });
+// };
 export const GenerateCookie = (session, res) => {
   res.cookie(COOKIE.REFRESH_TOKEN, session.refreshToken, {
     httpOnly: true,
+    secure: false, // Если вы тестируете на локальном хосте, установите false
+    sameSite: 'Lax',
     expires: new Date(Date.now() + TIME_DURATION.THIRTY_DAYS),
   });
 
   res.cookie(COOKIE.SESSION_ID, session.id, {
     httpOnly: true,
+    secure: false, // Если вы тестируете на локальном хосте, установите false
+    sameSite: 'Lax',
     expires: new Date(Date.now() + TIME_DURATION.THIRTY_DAYS),
   });
 };
