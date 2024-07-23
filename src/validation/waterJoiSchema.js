065c532be19a3329babf80a975c44f2191f71dcc
Rodion Matvieiev <your.email@example.com>
2024-07-23 00:39:38 -0400

    add/edit/delete - done

--- src/validation/waterJoiSchema.js
+++ src/validation/waterJoiSchema.js
@@ -1,14 +1,13 @@
 import Joi from 'joi';
 
 const addWaterSchema = Joi.object({
-  userId: Joi.string(),
   waterValue: Joi.number().required(),
   date: Joi.date().required(),
 });
 
 const editWaterSchema = Joi.object({
-  waterValue: Joi.number(),
-  time: Joi.string(),
+  waterValue: Joi.number().required(),
+  date: Joi.date().required(),
 });
 
 export const water = {
